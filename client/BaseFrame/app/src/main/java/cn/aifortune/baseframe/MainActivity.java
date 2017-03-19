2ee5384a330aedf78c78ebe1eb910b4c22af7cdd
GreekPanda <271799648@qq.com>
2017-03-19 19:11:52 +0800

    增加客户端代码基础框架，现在仅实现了最基本的UI设计模式

---
+++ client/BaseFrame/app/src/main/java/cn/aifortune/baseframe/MainActivity.java
@@ -0,0 +1,56 @@
+package cn.aifortune.baseframe;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab_layout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        initViewPager();
+    }
+
+    private void initViewPager() {
+        mTabLayout = (TabLayout) findViewById(R.id.tabs);
+        List<String> titles = new ArrayList<>();
+        titles.add("财经头条");
+        titles.add("涨停板");
+        titles.add("跌停板");
+        titles.add("智能预测");
+        titles.add("缠中说禅");
+
+
+        for (int i = 0; i < titles.size(); i++) {
+            mTabLayout.addTab(mTabLayout.newTab().setText(titles.get(i)));
+        }
+        List<Fragment> fragments = new ArrayList<>();
+        for (int i = 0; i < titles.size(); i++) {
+            fragments.add(new ListFragment());
+        }
+        FragmentAdapter mFragmentAdapteradapter =
+                new FragmentAdapter(getSupportFragmentManager(), fragments, titles);
+
+        mViewPager.setAdapter(mFragmentAdapteradapter);
+
+        mTabLayout.setupWithViewPager(mViewPager);
+
+        mTabLayout.setTabsFromPagerAdapter(mFragmentAdapteradapter);
+    }
+}
